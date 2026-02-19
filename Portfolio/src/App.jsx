d59bd837c9554058cd41e0db7aff7146af599eae
Ankit Yadav <ankit-yadav90@gmail.com>
2026-02-19 08:19:30 +0530

    create to the particales background

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,31 @@
-import { memo } from 'react';
+import Navbar from "./components/Navbar"
+import CoustomCursor from "./components/CoustomCursor"
+import ParticalesBackground from "./components/ParticalesBackground"
+import Home from "./sections/Home"
+import About from "./sections/About"
+import Skills from './sections/Skills'
+import Project from "./sections/Project"
+import Experence from "./sections/Experence"
+import Testmonials from "./sections/Testmonials"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 
-const App = () => {
-  return (
-    <div>
-      <h2>My Portfolio</h2>
-      
+export default function App(){
+  return(
+    <div className="relative gradient text-white">
+      <CoustomCursor></CoustomCursor>
+      <ParticalesBackground></ParticalesBackground>
+       <Navbar></Navbar>
+       <Home></Home>
+       <About></About>
+       <Skills></Skills>
+       <Project></Project>
+       <Experence></Experence>
+       <Testmonials></Testmonials>
+       <Contact></Contact>
+       <Footer></Footer>
+
+       keuch bhi
     </div>
-  );
-};
-
-export default memo(App);
+  )
+}
